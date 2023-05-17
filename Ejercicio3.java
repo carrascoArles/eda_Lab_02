2008acc60ad79859b18b63f59defa0ed8071b098
JeanChara <jcharaco@unsa.edu.pe>
2023-05-17 16:41:25 -0500

    añadido base para ejercicio 3

---
+++ Ejercicio3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ejercicio03{
+    public static void main (String [] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ingrese la altura del triangulo");
+        int altura = scan.nextInt();
+        imprimirTriangulo(altura);
+
+
+
+    }
+    public static void imprimirTriangulo(int altura){
+        if (){
+
+        }
+    }
+}
