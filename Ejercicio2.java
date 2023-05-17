e219520d9de0c6ce603399ed0868455136a2d956
Omar <oromero@unsa.edu.pe>
2023-05-17 17:19:41 -0500

    primer avance

---
+++ Ejercicio2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Ejercicio2 {
+	public static void main(String[] args) {
+		int [] array = {1, 2, 3, 4, 5};
+		
+        for (int i = 0; i < 2; i++) {
+            rotarIzquierdaArray(array);
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+    
+    public static int[] rotarIzquierdaArray(int[] A) {
+        //Procedimiento para rotar la matriz
+		
+        //return Aiz;
+        return A;
+    }
+}
