50fac7d001d17f11c7b3f8e6f2322caa32148148
carrascoArles <acarrascoch@unsa.edu.pe>
2023-05-18 10:31:41 -0500

    Mejorado casi acabado

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,11 +1,53 @@
-public int[] invertirArray(int[] Array){
-    int aux=0
+
+import java.util.*;
+
+public class Ejercicio1 {
+    
+    public static void main(String[] args) {
+        
+        int n;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ingrese la cantidad de numeros");
+        n=scan.nextInt();
+        int num[] =new int[n];
+        insertar(num);
+        
+        imprimir(num);
+        
+        invertirArray(num);
+                
+	}
+    
+public static int[] insertar(int[] Array){
+    int aux=0;
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Ingrese los numeros");
+    for (int i=0; i<Array.length; i++) {
+        Array[i]=scan.nextInt();
+    }
+      return Array;
+}
+    
+public static void invertirArray(int[] Array){
+    int aux=0;
     for (int i=0; i<Array.length/2; i++) {
         aux = Array[i];
         Array[i] = Array[Array.length-1-i];
         Array[Array.length-1-i] = aux;
     }
- 
+      imprimir(Array);
+}
+private static void imprimir(int[] array){
+        String nums="";
+        for (int i = 0; i < array.length; i++) {
+            nums += array[i] + ", ";
+        }
 
- return Array[];
+        System.out.println(nums);
+    }
+
+
+	  
+
+    
 }
