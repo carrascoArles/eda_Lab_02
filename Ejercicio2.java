ee6222f645905863fb19a9e4c98a666e144f66f6
Vladimir Jaward Hancco Soncco <117586735+vhanccos@users.noreply.github.com>
2023-05-21 22:03:17 +0000

    Actualiza a un ejercicio funcional

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,22 +1,36 @@
-import java.util.*;
-
 public class Ejercicio2 {
-	public static void main(String[] args) {
-		int [] array = {1, 2, 3, 4, 5};
-		
-        for (int i = 0; i < 2; i++) {
-            rotarIzquierdaArray(array);
-        }
+    public static void main(String[] args) {
+        final int d = 2;
+        int[] array = { 1, 2, 3, 4, 5 };
 
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
+        mostrarArray(array);
+
+        for (int i = 0; i < d; i++)
+            array = rotarIzquierdaArray(array);
+        
+        System.out.print(" -> ");
+        mostrarArray(array);
+        System.out.println();
     }
-    
+
+    public static void mostrarArray(int[] A) {
+        System.out.print("[ ");
+
+        for (int i = 0; i < A.length; i++)
+            System.out.print(A[i] + " ");
+        
+        System.out.print("]");
+    }
+
     public static int[] rotarIzquierdaArray(int[] A) {
-        //Procedimiento para rotar la matriz
-		
-        //return Aiz;
-        return A;
+        int tam = A.length;
+        int[] Aiz = new int[tam];
+
+        for (int i = 1; i < tam; i++)
+            Aiz[i - 1] = A[i];
+
+        Aiz[tam - 1] = A[0];
+
+        return Aiz;
     }
 }
