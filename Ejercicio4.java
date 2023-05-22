12883bdc29167eb60e9097d961916eb896a7508e
Joel Isaias Condori Leon <jcondorile@unsa.edu.pe>
2023-05-21 22:50:38 -0400

    Se agrego 2 variables

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,5 +1,13 @@
 package EjercicioCompleto4;
 
 public class Node<T> {
-
+   //La clase tiene dos variables mienbro privada
+    private T data;
+    private Node<T> nextNode;
+    
+    //Constructor que se utiliza para crear un nuevo objeto
+    public Node(T data) {
+        this.data = data;
+        this.nextNode = null;
+    }
 }
